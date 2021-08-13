dcdf59e143ad6480ddb93c10909ed0774fd30a57
Ws-Peroth <a01033056809@gmail.com>
2021-08-13 15:30:40 +0900

    Add EnemyView Script

---
+++ 2021KoreanYouthGamejam/Assets/Script_peroth/EnemyScripts/EnemyView.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class EnemyView : MonoBehaviour
+{
+
+    Image healthBar;
+    float maxHealth = 100f;
+    public static float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar = GetComponent<Image>();
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.fillAmount = health / maxHealth;
+    }
+}
