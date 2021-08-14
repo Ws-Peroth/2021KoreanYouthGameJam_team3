0f7e94fa2a424bd545c48f81ef3a14f7c2342899
SuperJoy <superjoy0502@gmail.com>
2021-08-14 15:36:23 +0900

    필요에 의해서 뒤로 빼세요

---
+++ 2021KoreanYouthGamejam/Assets/Script_peroth/EnemyScripts/RobotEnemy.cs
@@ -0,0 +1,11 @@
+﻿namespace peroth
+{
+    public class RobotEnemy : HumanEnemy
+    {
+        public override void PlayerApproachNear()
+        {
+            var player = target.GetComponent<Player>();
+            IsDetected(player);
+        }
+    }
+}
